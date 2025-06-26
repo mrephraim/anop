a001cbcc11289ca48a4ae246f98d211388ab84b5
Ephraim Agumbada <ephraimduchis@gmail.com>
2025-06-26 14:03:20 +0100

    Alpha Testing, Initial Commit vs 0.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.20"
-    id("com.gradleup.shadow") version "8.3.1"
+    id("com.gradleup.shadow") version "9.0.0-beta17"
 }
 
 group = "com.example"
